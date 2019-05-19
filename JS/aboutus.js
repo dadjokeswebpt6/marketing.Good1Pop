7d87737e19c208234f13a075536d145dd7730cd8
bbaney <48811486+bbaney@users.noreply.github.com>
2019-05-19 15:24:28 -0500

    added an image carousel to the mailinglist page

--- JS/aboutus.js
+++ JS/aboutus.js
@@ -11,3 +11,13 @@ navItems.forEach(function(item) {
         item.style.fontSize = '2rem';
     });
 });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
